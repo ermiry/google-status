35680348255a777c12235bbe714c57acceadca1e
ermiry <erick.salas@ermiry.com>
2021-06-18 23:44:58 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,9 +3,9 @@
 
 #define GOOGLE_VERSION				"0.1"
 #define GOOGLE_VERSION_NAME			"Version 0.1"
-#define GOOGLE_VERSION_DATE			"28/05/2021"
-#define GOOGLE_VERSION_TIME			"23:30 CST"
-#define GOOGLE_VERSION_AUTHOR			"Erick Salas"
+#define GOOGLE_VERSION_DATE			"18/06/2021"
+#define GOOGLE_VERSION_TIME			"23:44 CST"
+#define GOOGLE_VERSION_AUTHOR		"Erick Salas"
 
 // print full google version information
 extern void google_version_print_full (void);
